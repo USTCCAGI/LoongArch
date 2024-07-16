67ccb289b1f6062669509405fbcae764a1aaf400
fengli1702 <feng1702@mail.ustc.edu.cn>
2024-07-16 14:40:13 +0800

    mul div right

--- feng/Divide2.scala
+++ feng/Divide2.scala
@@ -4,41 +4,41 @@ import Control_Signal._
 
 class Divide2 extends Module{
     val io=IO(new Bundle{
-        val src1=Input(UInt(32.W))
-        val src2=Input(UInt(32.W))
+        val num1=Input(UInt(32.W))
+        val num2=Input(UInt(32.W))
         val op  =Input(UInt(5.W))
-        val res =Output(UInt(32.W))
+        val div_out =Output(UInt(32.W))
         val busy=Output(Vec(32, Bool()))//忙等线0123
     })
 
-    //第一段： 记录src2和op和sign，符号修正获得绝对值
+    //第一段： 记录num2和op和sign，符号修正获得绝对值
     val en = io.op(2)
     val sign = Wire(UInt(1.W))
 
     when(io.op === ALU_DIV){
-        sign := io.src1(31) ^ io.src2(31)
+        sign := io.num1(31) ^ io.num2(31)
     }.elsewhen(io.op === ALU_MOD){
-        sign := io.src1(31)
+        sign := io.num1(31)
     }.otherwise{
-        sign := 0.U
+        sign := false.B
     }
 
-    val src1 = Wire(UInt(32.W))
-    val src2 = Wire(UInt(32.W))
-    when((io.op === ALU_DIV || io.op === ALU_MOD) && io.src1(31)){
-        src1 := ~io.src1 + 1.U
+    val num1 = Wire(UInt(32.W))
+    val num2 = Wire(UInt(32.W))
+    when((io.op === ALU_DIV || io.op === ALU_MOD) && io.num1(31)){
+         num1 := ~io.num1 + 1.U
     }.otherwise{
-        src1 := io.src1
+        num1 := io.num1
     }
-    when((io.op === ALU_DIV || io.op === ALU_MOD) && io.src2(31)){
-        src2 := ~io.src2 + 1.U
+    when((io.op === ALU_DIV || io.op === ALU_MOD) && io.num2(31)){
+        num2 := ~io.num2 + 1.U
     }.otherwise{
-        src2 := io.src2
+        num2 := io.num2
     }
 
-    //获取src1中最高位的1，算一次除法需要多少个周期
+    //获取num1中最高位的1，算一次除法需要多少个周期
 
-    val high = PriorityEncoder(Reverse(src1))
+    val high = PriorityEncoder(Reverse(num1))
     val cnt = RegInit(VecInit.fill(32)(0.U(6.W)))
     val en_reg1 = ShiftRegister(en, 1, !io.busy(3))
     val high_reg1 = ShiftRegister(high, 1, !io.busy(3))
@@ -51,20 +51,20 @@ class Divide2 extends Module{
     }
 
     //多段流水
-    val src1_reg1 = ShiftRegister(src1, 1, !io.busy(0))
-    val src2_reg1 = ShiftRegister(src2, 1, !io.busy(1))
+    val num1_reg1 = ShiftRegister(num1, 1, !io.busy(0))
+    val num2_reg1 = ShiftRegister(num2, 1, !io.busy(1))
 
     val op_reg1 = ShiftRegister(io.op, 1, !io.busy(2))
     val sign_reg1 = ShiftRegister(sign, 1, !io.busy(2))
 
-    val src2_reg2 = RegInit(0.U(32.W))
+    val num2_reg2 = RegInit(0.U(32.W))
     val op_reg2 = RegInit(0.U(5.W))
     val sign_reg2 = RegInit(0.U)
 
     //第二段输出，计算结果
     when(en_reg1 && cnt(1) === 0.U){
         op_reg2 := op_reg1
-        src2_reg2 := src2_reg1
+        num2_reg2 := num2_reg1
         sign_reg2 := sign_reg1
     }
 
@@ -73,13 +73,13 @@ class Divide2 extends Module{
     //第三段： 除法计算
     val q_r = RegInit(0.U(65.W))
     when(cnt(1) =/= 0.U){
-        when(q_r(63,32)>=src2_reg2){
-            q_r := (q_r(63,32)-src2_reg2) ## q_r(31,0) ## 1.U(1.W)
+        when(q_r(63,32)>=num2_reg2){
+            q_r := (q_r(63,32)-num2_reg2) ## q_r(31,0) ## 1.U(1.W)
         }.otherwise{
             q_r := (q_r(63,0) ## 0.U(1.W))
         }
     }.elsewhen(en_reg1){//除法初始化，被除数绝对值放到RQ低32位
-        q_r := 0.U(33.W) ## src1_reg1 << high_reg1
+        q_r := (0.U(33.W) ## num1_reg1 )<< high_reg1
     } 
 
     val res = Wire(UInt(32.W))
@@ -101,6 +101,6 @@ class Divide2 extends Module{
         res := q_r(64,33)
     }
 
-    io.res := res
+    io.div_out := res
 
 }
--- feng/Multiply2.scala
+++ feng/Multiply2.scala
@@ -2,63 +2,61 @@ import chisel3._
 import chisel3.util._
 import Control_Signal._
 
-
+//feng
 class Multiply2 extends Module{
     val io = IO(new Bundle{
-        val src1 = Input(UInt(32.W))
-        val src2 = Input(UInt(32.W))
+        val num1 = Input(UInt(32.W))
+        val num2 = Input(UInt(32.W))
         val op   = Input(UInt(5.W))
-        val res  = Output(UInt(32.W))
+        val mul_out  = Output(UInt(32.W))
         val busy = Input(Vec(32, Bool()))//来自div
         
     })
 
-    val src1 = Wire(UInt(66.W))
-    val src2 = Wire(UInt(66.W))
-    src1 := 0.U
-    src2 := 0.U
+    val num1 = Wire(UInt(66.W))
+    val num2 = Wire(UInt(66.W))
+    num1 := 0.U
+    num2 := 0.U
 
     when(io.op === ALU_MULHU){
-        src1 := 0.U(34.W) ## io.src1
-        src2 := 0.U(34.W) ## io.src2
+        num1 := 0.U(34.W) ## io.num1
+        num2 := 0.U(34.W) ## io.num2
     }.otherwise{
-        src1 := Fill(34, io.src1(31)) ## io.src1
-        src2 := Fill(34, io.src2(31)) ## io.src2
+        num1 := Fill(34, io.num1(31)) ## io.num1
+        num2 := Fill(34, io.num2(31)) ## io.num2
     }
 
     //Booth编码，使用乘数将被乘数编码
+    val Num_x = Wire(Vec(33, UInt(66.W)))//x
     val booth  = Wire(Vec(33, UInt(3.W)))
-
-    booth(0)  := Cat(src2(1),  src2(0),  0.U) // 最低位B(-1)=0
+    booth(0)  := num2(1) ## num2(0) ##  0.U // 最低位B(-1)=0
     for(i<-1 until 33){
-        booth(i) := Cat(src2(2*i+1), src2(2*i-1))
+        booth(i) := num2(2*i+1) ## num2(2*i) ## num2(2*i-1)
     }
 
-    val Num_x = Wire(Vec(33, UInt(66.W)))//x
-
-    //计算华莱士树输入。33个输入，每个输入66位
     for (i <- 0 until 33) {
-        Num_x(i) := 0.U
-
-        when(booth(i) === 0.U || booth(i) === 7.U) {
-            Num_x(i) := 0.U       
-        }.elsewhen(booth(i)===1.U || booth(i)===2.U){
-            Num_x(i) := Cat(src1(65-2*i, 0),0.U((2*i).W))
+        val temp = Cat(num1(65-2*i, 0),0.U((2*i).W))
+        when(booth(i)===1.U || booth(i)===2.U){
+            Num_x(i) := temp
         }.elsewhen(booth(i)===3.U){
-            Num_x(i) := (Cat(src1(65-2*i, 0),0.U((2*i).W)))<<1
+            Num_x(i) := ((temp)<<1)
         }.elsewhen(booth(i)===4.U){
-            Num_x(i) := ~((Cat(src1(65-2*i, 0),0.U((2*i).W)))<<1)+1.U
+            Num_x(i) := (~(temp<<1)+1.U)
         }.elsewhen(booth(i)===5.U || booth(i)===6.U){
-            Num_x(i) := ~((Cat(src1(65-2*i, 0),0.U((2*i).W))))+1.U
+            Num_x(i) := (~(temp)+1.U)
+        }.otherwise{
+            Num_x(i) := 0.U
         }
+
     }
     //第一寄存器
+   
     val Num_reg1 = ShiftRegister(Num_x, 1, !io.busy(6))
     //保留进位加法器
     def CSA(x: UInt, y: UInt, z: UInt) : UInt  = {
-        val res1 = x ^ y ^ z
-        val res2 = ((((x & y) | z & (x ^ y)) << 1)(65, 0))
-        res2 ## res1
+        val mul_out1 = (x ^ y) ^ z
+        val mul_out2 = ((((x & y) | z & (x ^ y)) << 1)(65, 0))
+        mul_out2 ## mul_out1
     }
 
     //华莱士树
@@ -83,7 +81,7 @@ class Multiply2 extends Module{
     val input_3 =  Wire(Vec(5, UInt(132.W)))
     val output_3 = Wire(Vec(10, UInt(66.W)))
     for(i<-0 until 5){
-        input_3(i) := CSA(output_2(2*i), output_2(2*i+1), output_2(2*i+2))
+        input_3(i) := CSA(output_2(3*i), output_2(3*i+1), output_2(3*i+2))
         output_3(2*i) := input_3(i)(65,0)
         output_3(2*i+1) := input_3(i)(131,66)
     }
@@ -91,7 +89,7 @@ class Multiply2 extends Module{
     val input_4 =  Wire(Vec(3, UInt(132.W)))
     val output_4 = Wire(Vec(7, UInt(66.W)))
     for(i<-0 until 3){
-        input_4(i) := CSA(output_3(2*i), output_3(2*i+1), output_3(2*i+2))
+        input_4(i) := CSA(output_3(3*i), output_3(3*i+1), output_3(3*i+2))
         output_4(2*i) := input_4(i)(65,0)
         output_4(2*i+1) := input_4(i)(131,66)
     }
@@ -100,7 +98,7 @@ class Multiply2 extends Module{
     val input_5 =  Wire(Vec(2, UInt(132.W)))
     val output_5 = Wire(Vec(5, UInt(66.W)))
     for(i<-0 until 2){
-        input_5(i) := CSA(output_4(2*i), output_4(2*i+1), output_4(2*i+2))
+        input_5(i) := CSA(output_4(3*i), output_4(3*i+1), output_4(3*i+2))
         output_5(2*i) := input_5(i)(65,0)
         output_5(2*i+1) := input_5(i)(131,66)
     }
@@ -109,7 +107,7 @@ class Multiply2 extends Module{
     val input_6 =  Wire(Vec(1, UInt(132.W)))
     val output_6 = Wire(Vec(4, UInt(66.W)))
     for(i<-0 until 1){
-        input_6(i) := CSA(output_5(2*i), output_5(2*i+1), output_5(2*i+2))
+        input_6(i) := CSA(output_5(3*i), output_5(3*i+1), output_5(3*i+2))
         output_6(2*i) := input_6(i)(65,0)
         output_6(2*i+1) := input_6(i)(131,66)
     }
@@ -119,7 +117,7 @@ class Multiply2 extends Module{
     val input_7 =  Wire(Vec(1, UInt(132.W)))
     val output_7 = Wire(Vec(3, UInt(66.W)))
     for(i<-0 until 1){
-        input_7(i) := CSA(output_6(2*i), output_6(2*i+1), output_6(2*i+2))
+        input_7(i) := CSA(output_6(3*i), output_6(3*i+1), output_6(3*i+2))
         output_7(2*i) := input_7(i)(65,0)
         output_7(2*i+1) := input_7(i)(131,66)
     }
@@ -128,21 +126,21 @@ class Multiply2 extends Module{
     val input_8 =  Wire(Vec(1, UInt(132.W)))
     val output_8 = Wire(Vec(2, UInt(66.W)))
     for(i<-0 until 1){
-        input_8(i) := CSA(output_7(2*i), output_7(2*i+1), output_7(2*i+2))
+        input_8(i) := CSA(output_7(3*i), output_7(3*i+1), output_7(3*i+2))
         output_8(2*i) := input_8(i)(65,0)
         output_8(2*i+1) := input_8(i)(131,66)
     }
     //输出
     //三段流水输出
-    val res1 = ShiftRegister(output_8(0), 1, !io.busy(7))
-    val res2 = ShiftRegister(output_8(1), 1, !io.busy(8))
+    val mul_out1 = ShiftRegister(output_8(0)(63, 0), 1, !io.busy(7))
+    val mul_out2 = ShiftRegister(output_8(1)(63, 0), 1, !io.busy(8))
     val op_reg1      = ShiftRegister(io.op, 1, !io.busy(9))
     val op_reg2      = ShiftRegister(op_reg1, 1, !io.busy(10))
-    val result = res1 + res2
+    val mul_out = mul_out1 + mul_out2
 
-    when(io.op === ALU_MUL){
-        io.res := result(31,0)
+    when(op_reg2  === ALU_MUL){
+        io.mul_out := mul_out(31,0)
     }.otherwise{
-        io.res := result(63,32)
+        io.mul_out := mul_out(63,32)
     }
 }
