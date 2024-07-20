f5a6f97fda090638af2fac254aa4586a429450ec
lancelot <1532833931@qq.com>
2024-07-20 17:48:26 +0800

    modify decoder

--- src/main/scala/00_chang/04-Decode-Stage/Decode.scala
+++ src/main/scala/00_chang/04-Decode-Stage/Decode.scala
@@ -1,6 +1,7 @@
 import chisel3._
 import chisel3.util._
 import Decode_Map._
+import Control_Signal._
 
 class DecodeIO extends Bundle{
     val inst            = Input(UInt(32.W))
@@ -24,53 +25,64 @@ class DecodeIO extends Bundle{
 }
 class Decode extends Module{
     val io = IO(new DecodeIO)
-    val signal = ListLookup(io.inst, Decode_Map.default, Decode_Map.map)
+    val control_signal = ListLookup(io.inst, Decode_Map.default, Decode_Map.map)
 
-    io.rj               := Mux(signal(0).asBool, io.inst(9, 5), 0.U(5.W))
-
-    io.rk               := Mux(signal(1).asBool, Mux(signal(9)(0).asBool, io.inst(14, 10), io.inst(4, 0)), 0.U)
-
-    io.rd               := MuxLookup(signal(10), io.inst(4, 0))(Seq(
-                            Control_Signal.RD    -> io.inst(4, 0),
-                            Control_Signal.R1    -> 1.U(5.W),
-                            Control_Signal.RJ    -> io.inst(9, 5)))
-    io.rd_valid         := signal(2) & io.rd =/= 0.U(5.W)
-
-    io.alu_op           := signal(3)
-    io.alu_rs1_sel      := signal(4)
-    io.alu_rs2_sel      := signal(5)
-
-    io.br_type          := signal(6)
-    io.mem_type         := signal(7)
-
-    io.priv_vec         := signal(12)
-    
-    io.fu_id            := signal(8)
-    io.exception        := Mux(signal(12)(8) && io.inst(4, 0) >= 7.U, 1.U ## Control_Signal.INE, signal(13))
-
-
-    def Imm_Gen(inst: UInt, imm_type: UInt): UInt = {
-        val imm = Wire(UInt(32.W))
-        import Control_Signal._
-        imm := DontCare
-        switch(imm_type) {
-            is(IMM_00U)     { imm := 0.U(32.W) }
-            is(IMM_05U)     { imm := Cat(0.U(27.W), inst(14, 10)) }
-            is(IMM_12U)     { imm := Cat(0.U(20.W), inst(21, 10)) }
-            is(IMM_12S)     { imm := Cat(Fill(20, inst(21)), inst(21, 10)) }
-            is(IMM_14S)     { imm := Cat(Fill(18, inst(21)), inst(21, 10), 0.U(2.W)) }
-            is(IMM_16S)     { imm := Cat(Fill(14, inst(25)), inst(25, 10), 0.U(2.W)) }
-            is(IMM_20S)     { imm := Cat(inst(24, 5), 0.U(12.W)) }
-            is(IMM_26S)     { imm := Cat(Fill(4, inst(9)), inst(9, 0), inst(25, 10), 0.U(2.W)) }
-            is(IMM_CSR)     { imm := Cat(0.U(18.W), inst(23, 10)) }
-            is(IMM_TID)     { imm := 0x40.U(32.W) }
-            is(IMM_ERA)     { imm := 0x6.U(32.W) }
-            is(IMM_COP)     { imm := Cat(Fill(15, inst(21)), inst(21, 10), inst(4, 0))}
-        }
-        imm
+    when(control_signal(0).asBool){
+        io.rj := io.inst(9, 5)
+    }.otherwise{
+        io.rj := 0.U
     }
 
-    io.imm              := Imm_Gen(io.inst, signal(11))
+    when(control_signal(1).asBool){
+        io.rk := Mux(control_signal(9)(0).asBool, io.inst(14, 10), io.inst(4, 0))
+    }.otherwise(
+        io.rk := 0.U
+    )
+
+    io.rd := io.inst(4,0)
+    switch(control_signal(10)){
+        is(Control_Signal.RD){
+            io.rd := io.inst(4, 0)
+        }
+        is(Control_Signal.R1){
+            io.rd := 1.U
+        }
+        is(Control_Signal.RJ){
+            io.rd := io.inst(9, 5)
+        }
+    }
+
+    io.rd_valid := control_signal(2).asBool && (io.rd =/= 0.U(5.W))
+
+    io.alu_op := control_signal(3)
+    io.alu_rs1_sel := control_signal(4)
+    io.alu_rs2_sel := control_signal(5)
+
+    io.br_type := control_signal(6)
+    io.mem_type := control_signal(7)
+
+    io.priv_vec := control_signal(12)
+    
+    io.fu_id := control_signal(8)
+    io.exception := control_signal(13)
+
+    val imm = Wire(UInt(32.W))
+    imm := DontCare
+    switch(control_signal(11)){
+        is(IMM_00U){imm := 0.U(32.W)}
+        is(IMM_05U){imm := 0.U(27.W) ## io.inst(14, 10)}
+        is(IMM_12U){imm := 0.U(20.W) ## io.inst(21, 10)}
+        is(IMM_12S){imm := Fill(20, io.inst(21)) ## io.inst(21, 10)}
+        is(IMM_14S){imm := Fill(18, io.inst(21)) ## io.inst(21, 10) ## 0.U(2.W)}
+        is(IMM_16S){imm := Fill(14, io.inst(25)) ## io.inst(25, 10) ## 0.U(2.W)}
+        is(IMM_20S){imm := io.inst(24, 5) ## 0.U(12.W)}
+        is(IMM_26S){imm := Fill(4, io.inst(9)) ## io.inst(9, 0) ## io.inst(25, 10) ## 0.U(2.W)}
+        is(IMM_CSR){imm := 0.U(18.W) ## io.inst(23, 10)}
+        is(IMM_TID){imm := 0x40.U(32.W)}
+        is(IMM_ERA){imm := 0x6.U(32.W)}
+    }
+
+    io.imm := imm
 }
 
 
