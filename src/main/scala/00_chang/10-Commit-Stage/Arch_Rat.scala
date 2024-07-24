977535de9a8a3ef7072d3ac0c6e6f820bf9c832c
lancelot <1532833931@qq.com>
2024-07-24 11:23:34 +0800

    dbg compling bugs

--- src/main/scala/00_chang/10-Commit-Stage/Arch_Rat.scala
+++ src/main/scala/00_chang/10-Commit-Stage/Arch_Rat.scala
@@ -24,50 +24,39 @@ class Arch_Rat_IO(n: Int) extends Bundle {
     val ras_arch            = Output(Vec(8, UInt(32.W)))
 }
 
-
 class Arch_Rat(n: Int) extends Module {
     val io = IO(new Arch_Rat_IO(n))
 
     val arat = RegInit(VecInit.fill(n)(false.B))
-    
-    //update_arat
-    val arat_next = Wire(Vec(n, Bool()))
+
     val head = RegInit(0.U(log2Ceil(n).W))
     var head_next = head
-    val update_arat = Wire(Vec(2, Bool()))
     for(i <- 0 until 2){
-        update_arat(i) := io.rd_valid_cmt(i) && io.cmt_en(i)
-        when(update_arat(i)){
+        when(io.cmt_en(i) && io.rd_valid_cmt(i)){
             head_next = Mux(head_next === (n-1).U, 0.U, head_next + 1.U)
+            arat(io.pprd_cmt(i)) := false.B
+            arat(io.prd_cmt(i)) := true.B
         }
     }
     head := head_next
 
-    for(i <- 0 until 2){
-        when(update_arat(i)){
-            arat(io.prd_cmt(i)) := true.B
-            arat(io.pprd_cmt(i)) := false.B
-        }
-    } 
+    io.arch_rat     := arat
+    io.head_arch    := head
 
-    io.arch_rat := arat
-    io.head_arch := head
 
-    //ras
-    val top = RegInit(0x7.U(3.W))
-    val return_address_stack = RegInit(VecInit.fill(8)(0x1c000000.U(32.W)))
-
-    val update_pop = (io.br_type_pred_cmt === RET) && (io.pred_update_en_cmt)
-    val update_push = io.br_type_pred_cmt(1) && io.pred_update_en_cmt
-
-    when(update_pop){
+    // ras
+    val top         = RegInit(0x7.U(3.W))
+    val ras         = RegInit(VecInit.fill(8)(0x1c000000.U(32.W)))
+    val stack_pop  = io.br_type_pred_cmt === RET && io.pred_update_en_cmt
+    val stack_push = io.br_type_pred_cmt(1) && io.pred_update_en_cmt
+    when(stack_pop){
         top := top - 1.U
-    }.elsewhen(update_push){
+    }.elsewhen(stack_push){
         top := top + 1.U
-        return_address_stack(top) := io.pc_cmt
+        ras(top) := io.pc_cmt
     }
-
+    
     io.top_arch := top
-    io.ras_arch := return_address_stack
+    io.ras_arch := ras
 
 }
