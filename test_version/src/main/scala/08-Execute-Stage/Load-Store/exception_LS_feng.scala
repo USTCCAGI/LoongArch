29400363e41feed7f326ad3e59df1ee9db820545
fengli1702 <feng1702@mail.ustc.edu.cn>
2024-08-04 17:56:31 +0800

    feng csr

--- test_version/src/main/scala/08-Execute-Stage/Load-Store/exception_LS_feng.scala
+++ test_version/src/main/scala/08-Execute-Stage/Load-Store/exception_LS_feng.scala
@@ -1,21 +1,32 @@
+import Exception._
 import chisel3._
 import chisel3.util._
-import Exception._
-class Exception_LS_IO extends Bundle{
-    val addr_EX          = Input(UInt(32.W))
-    val mem_type_EX      = Input(UInt(5.W))
-    val exception_ls     = Output(UInt(8.W))
+
+class Exception_LS_IO extends Bundle {
+  val addr_EX = Input(UInt(32.W))
+  val mem_type_EX = Input(UInt(5.W))
+  val exception_ls = Output(UInt(8.W))
 }
 
-class Exception_LS extends Module{
-    val io = IO(new Exception_LS_IO)
-    val addr_EX          = io.addr_EX
-    val mem_type         = io.mem_type_EX
+class Exception_LS extends Module {
+  val io = IO(new Exception_LS_IO)
+  val addr_EX = io.addr_EX
+  val mem_type = io.mem_type_EX
 
-    val exception_ls = Wire(UInt(8.W))
+  val exception_ls = Wire(UInt(8.W))
+  exception_ls := 0.U(8.W)
+  val check = WireDefault(0.U(4.W))
 
-    val mem_mask = (1.U(4.W) << mem_type(1, 0)) - 1.U
-    exception_ls := Mux((addr_EX & mem_mask) =/= 0.U, 1.U(1.W) ## ALE, 0.U)
+  switch(mem_type(1, 0)) {
+    is(0.U) { check := "b0001".U(4.W) }  // 1-byte
+    is(1.U) { check := "b0011".U(4.W) }  // 2-byte
+    is(2.U) { check := "b0111".U(4.W) }  // 4-byte
+    is(3.U) { check := "b1111".U(4.W) }  // 8-byte
+  }
 
-    io.exception_ls := exception_ls
+  val alignment_exception = (addr_EX & check) =/= 0.U
+
+  exception_ls := Mux(alignment_exception, 1.U(1.W) ## ALE, 0.U)
+
+  io.exception_ls := exception_ls
 }
