37c26d4c061f1e4aac34c79b08c7ed7836cd5b71
feng <119935962+fengli1702@users.noreply.github.com>
2024-08-05 09:13:53 +0800

    Update exception_LS_feng.scala

--- test_version/src/main/scala/08-Execute-Stage/Load-Store/exception_LS_feng.scala
+++ test_version/src/main/scala/08-Execute-Stage/Load-Store/exception_LS_feng.scala
@@ -12,10 +12,31 @@ class Exception_LS extends Module{
     val addr_EX          = io.addr_EX
     val mem_type         = io.mem_type_EX
 
-    val exception_ls = Wire(UInt(8.W))
+    //val exception_ls = Wire(UInt(8.W))
 
-    val mem_mask = (1.U(4.W) << mem_type(1, 0)) - 1.U
-    exception_ls := Mux((addr_EX & mem_mask) =/= 0.U, 1.U(1.W) ## ALE, 0.U)
+    //val mem_mask = (1.U(4.W) << mem_type(1, 0)) - 1.U
+    //exception_ls := Mux((addr_EX & mem_mask) =/= 0.U, 1.U(1.W) ## ALE, 0.U)
 
-    io.exception_ls := exception_ls
+    val check_mask = WireDefault(0.U(4.W))
+    switch (mem_type(1, 0)){
+        is(0.U){
+            check_mask := 0.U
+        }
+        is(1.U){
+            check_mask := 1.U
+        }
+        is(2.U){
+            check_mask := 3.U
+        }
+        is(3.U){
+            check_mask := 7.U
+        }
+    }
+
+    when((addr_EX & check_mask) =/= 0.U(32.W)){
+        io.exception_ls := 1.U(1.W) ## ALE
+    }.otherwise{
+        io.exception_ls := 0.U
+    }
+
 }
